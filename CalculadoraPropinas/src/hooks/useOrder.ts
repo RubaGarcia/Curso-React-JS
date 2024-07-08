3f958fca221143f7b3a08a3d62b096da8ebe9c79
DESKTOP-UKC22T0\Incentro <ruben.garcia@incentro.com>
2024-07-08 15:34:24 +0200

    acabado proyecto de calculadora de propinas

---
+++ CalculadoraPropinas/src/hooks/useOrder.ts
@@ -0,0 +1,42 @@
+import { useState } from "react"
+import type { MenuItem, OrderItem } from "../types"
+
+export default function useOrder(){
+
+    const[order, setOrder] =useState<OrderItem[]>([])
+    const[tip,setTip] = useState(0)
+
+    const addItem =(item:MenuItem) =>{
+        // console.log(item)
+        const itemExists = order.find(orderItem => orderItem.id === item.id)
+        if(itemExists){
+            const upOrder=order.map(orderItem => orderItem.id===item.id ?
+                {...orderItem, quantity:orderItem.quantity+1} :
+                orderItem
+            )
+            setOrder(upOrder)
+        }else{
+            const newItem:OrderItem = {...item, quantity:1}
+            setOrder([...order, newItem])
+
+        }
+    }
+    const removeItem= (id:MenuItem['id']) => {
+        setOrder(order.filter(item => item.id !== id))
+    }
+
+    const placeOrder= ()=>{
+        setOrder([])
+        setTip(0)
+    }
+    
+    return {
+        order,
+        tip,
+        setTip,
+        addItem,
+        removeItem,
+        placeOrder
+    }
+    
+}
