703eb5401fba76aaa11be8a26464f616a58cf604
Rubén García <rgm177@alumnos.unican.es>
2024-07-18 14:22:59 +0200

    acabada seccion 19

---
+++ rest-api-node-ts-server/src/handlers/product.ts
@@ -0,0 +1,94 @@
+import { Request, Response } from 'express';
+import Product from '../models/Product.model';
+
+
+export const createProduct = async (req :Request,res:Response) => {
+
+
+    try {
+        const product = await Product.create(req.body)
+    
+        res.json({data: product})
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export const getProducts = async (req:Request, res:Response) => {
+    try {
+        const products = await Product.findAll({
+            order: [
+                ['id', 'DESC']
+            ],
+        })
+        res.json({data: products})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export const getProductById = async (req:Request, res:Response) => {
+    try {
+        const {id} = req.params
+        const product = await Product.findByPk(id)
+        if(!product){
+            return res.status(404).json({
+                msg: 'Product not found'
+            })
+        }
+        res.json({data: product})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const updateProduct = async (req:Request, res:Response) => {
+    const {id} = req.params	
+    const product = await Product.findByPk(id)
+
+    if(!product){
+        return res.status(404).json({
+            error: 'Product not found'
+        })
+    }
+    await product.update(req.body)
+    await product.save()
+
+    res.json({data: product})
+    // console.log(req.body)
+
+}
+
+export const updateAvailability = async (req: Request, res: Response) => {
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+
+    if(!product) {
+        return res.status(404).json({
+            error: 'Producto No Encontrado'
+        })
+    }
+    
+    // Actualizar
+    product.availability = !product.dataValues.availability
+    await product.save()
+    res.json({data: product})
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    const { id } = req.params
+    const product = await Product.findByPk(id)
+
+    if(!product) {
+        return res.status(404).json({
+            error: 'Producto No Encontrado'
+        })
+    }
+    
+    // Actualizar
+    await product.destroy()
+    res.json({data: product})
+}
---
+++ rest-api-node-ts-server/src/router.ts
@@ -0,0 +1,52 @@
+import { Router } from "express"
+import { createProduct, deleteProduct, getProductById, getProducts, updateAvailability, updateProduct } from "./handlers/product"
+import {body, param} from 'express-validator'
+import { handleInputErrors } from "./middleware"
+
+
+const router = Router()
+
+router.get("/",getProducts)
+router.get("/:id",
+    param('id').isInt().withMessage('Id must be an integer'),
+    handleInputErrors,
+    getProductById
+)
+
+router.post("/",
+    body('name')
+                .notEmpty().withMessage('Name is required'),
+    body('price')
+                .isNumeric().withMessage('Valir no valido')
+                .notEmpty().withMessage('Price is required')
+                .custom(value=> value > 0).withMessage('Price must be greater than 0'),
+    handleInputErrors,
+    createProduct
+)
+
+router.put("/:id",
+    body('name')
+                .notEmpty().withMessage('Name is required'),
+    body('price')
+                .isNumeric().withMessage('Valir no valido')
+                .notEmpty().withMessage('Price is required')
+                .custom(value=> value > 0).withMessage('Price must be greater than 0'),
+    body('availability')
+                .isBoolean().withMessage('Availability must be a boolean'),
+    handleInputErrors,
+    updateProduct
+)
+
+router.patch('/:id', 
+    param('id').isInt().withMessage('ID no válido'),
+    handleInputErrors,
+    updateAvailability
+)
+
+router.delete('/:id', 
+    param('id').isInt().withMessage('ID no válido'),
+    handleInputErrors,
+    deleteProduct
+)
+
+export default router
